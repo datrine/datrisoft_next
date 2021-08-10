df97b68a99ecfa04be2b6de2bcc61f52ea35d529
datrine <trinitietp@gmail.com>
2021-08-10 12:13:37 +0100

    retry

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import PCIndex from "../comps/page_views/index/pcIndex"
 import View from "../comps/view"
 
 const Home = () => {
-return <View mobileView={<MobileView/> } pcView={<PCView/>} />
+  return <View mobileView={<MobileView />} pcView={<PCView />} />
 }
 
 function MobileView(params) {
-  
+
 }
 
 function PCView(params) {
-  return<>
-  <PCIndex/>
+  return <>
+    <PCIndex />
   </>
 }
 
